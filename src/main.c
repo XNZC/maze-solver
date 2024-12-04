973615c8ca532fd8b401499fc2ec17822146bce1
X <100195228+XNZC@users.noreply.github.com>
2024-12-04 17:26:01 +0200

    -

--- src/main.c
+++ src/main.c
@@ -4,13 +4,13 @@
 
 
 int main(void){
-    BMPImage* image = read_image("../4.bmp");
+    BMPImage* image = read_image("../bitmaps/4.bmp");
     Maze* maze = bmp_to_maze(image);
 
     wall_follow(maze);
     BMPImage* solved_image = maze_to_bmp(maze);
 
-    bool success = write_image("../solved.bmp", solved_image);
+    bool success = write_image("../bitmaps/solved.bmp", solved_image);
 
     free_maze(maze);
     free_image(image);
